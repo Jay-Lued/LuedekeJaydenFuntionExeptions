c79a58e7ad96ea5481fa71e856c0f2b4dee6710f
Jayden Luedeke <jayden.luedeke@sinclair.edu>
2023-05-05 23:25:29 -0400

    Finished code project

---
+++ LuedekeJaydenFuntionExeptions/Source.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+
+using namespace std;
+
+class NotLetter_C
+{
+public:
+	string Throw_Not_Letter()
+	{
+		return ("Start character is not a letter: re-enter number");
+	}
+};
+
+class NotRange_C
+{
+public:
+	string Out_Of_Bounds()
+	{
+		return ("New character is out of bounds: re-enter number");
+	}
+};
+
+
+
+char character(char& start, int offset);
+
+int main()
+{
+	char choice;
+	int offset;
+	bool again = true;
+
+	while (again)
+	{
+		cout << "Please enter starting letter: ";
+		cin >> choice;
+		cout << "Please enter offset: ";
+		cin >> offset;
+
+		try
+		{
+			character(choice, offset);
+
+			cout << endl << "New letter: " << choice;
+
+			cout << endl << "Again? 1 for yes, 0 for no: ";
+			cin >> again;
+		}
+		catch (NotLetter_C& msg)
+		{
+			cout << msg.Throw_Not_Letter();
+		}
+		catch (NotRange_C& msg)
+		{
+			cout << msg.Out_Of_Bounds();
+		}
+	}
+
+	return 0;
+}
+
+char character(char& start, int offset)
+{
+	if ((start < 65 || start > 90) && (start < 97 || start > 122))
+	{
+		throw NotLetter_C();
+	}
+
+	start += offset;
+
+	if ((start < 65 || start > 90) && (start < 97 || start > 122))
+	{
+		throw NotRange_C();
+	}
+
+	return start;
+}
